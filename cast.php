b0423d50a12db9caee7918cd7ba591b7e1e2835e
eddie <edwina.landale@live.com.au>
2017-09-21 18:07:49 +0200

    changed html to php and created external nav file

---
+++ cast.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Synecdoche, New York - Cast and Crew</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M"
+    crossorigin="anonymous">
+<link rel="stylesheet" href="css/mymovie.css">
+<link rel="icon" type="image/png" href="img/favicon.png">
+</head>
+
+
+<body>
+    <?php include 'nav.php'; ?>
+        
+        
+    <div class="container leading_cast">
+        <h4 class="cast">Leading Cast:</h4>
+
+        <div class="row">
+            <div class="card-group">
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/psh.jpg" alt="Philip Seymour Hoffman">
+                        <div class="card-body">
+                            <h5 class="card-title">Philip Seymour Hoffman</h5>
+                            <p class="card-text">As: Caden Cotard</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/catherine_k.jpg" alt="Catherine Keener">
+                        <div class="card-body">
+                            <h5 class="card-title">Catherine Keener</h5>
+                            <p class="card-text">As: Adele Lack</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/sadie_g.jpg" alt="Sadie Goldstein">
+                        <div class="card-body">
+                            <h5 class="card-title">Sadie Goldstein</h5>
+                            <p class="card-text">As: Olive (4 years old)</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/tom_n.jpg" alt="Tom Noonan">
+                        <div class="card-body">
+                            <h5 class="card-title">Tom Noonan</h5>
+                            <p class="card-text">As: Sammy Barnathan</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/michelle_w.jpg" alt="Michelle Williams">
+                        <div class="card-body">
+                            <h5 class="card-title">Michelle Williams</h5>
+                            <p class="card-text">As: Claire Keen</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/samantha_m.jpg" alt="Samantha Morton">
+                        <div class="card-body">
+                            <h5 class="card-title">Samantha Morton</h5>
+                            <p class="card-text">As: Hazel</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/hope_d.jpg" alt="Hope Davis">
+                        <div class="card-body">
+                            <h5 class="card-title">Hope Davis</h5>
+                            <p class="card-text">As: Madeline Gravis</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/jennifer_jl.jpeg" alt="Jennifer Jason Leigh">
+                        <div class="card-body">
+                            <h5 class="card-title">Jennifer Jason Leigh</h5>
+                            <p class="card-text">As: Maria</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/robin_w.jpg" alt="Robin Weigert">
+                        <div class="card-body">
+                            <h5 class="card-title">Robin Weigert</h5>
+                            <p class="card-text">As: Adult Olive</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/emily_w.jpg" alt="Emily Watson">
+                        <div class="card-body">
+                            <h5 class="card-title">Emily Watson</h5>
+                            <p class="card-text">As: Tammy</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-3">
+                    <div class="card">
+                        <img class="card-img-top" src="img/dianne_w.jpg" alt="Dianne Wiest">
+                        <div class="card-body">
+                            <h5 class="card-title">Dianne Wiest</h5>
+                            <p class="card-text">As: Ellen Bascomb / Millicent Weems</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <h4 class="cast">Supporting Cast:</h4>
+        <div class="card card-columns" style="width: 20rem;">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Peter Friedman</li>
+                <li class="list-group-item">Daniel London</li>
+                <li class="list-group-item">Robert Seay</li>
+                <li class="list-group-item">Stephen Adly Guirgis</li>
+                <li class="list-group-item">Charles Techman</li>
+                <li class="list-group-item">Josh Pais</li>
+
+                <li class="list-group-item">Frank Girardeau</li>
+                <li class="list-group-item">Amy Wright</li>
+                <li class="list-group-item">Paul Sparks</li>
+                <li class="list-group-item">Jerry Adler</li>
+                <li class="list-group-item">Lynn Cohen</li>
+                <li class="list-group-item">Deirdre O'Connell</li>
+                <li class="list-group-item">Kat Peters</li>
+                <li class="list-group-item">John Rothman</li>
+                <li class="list-group-item">Amanda R. Phillips</li>
+                <li class="list-group-item">Frank Wood</li>
+                <li class="list-group-item">Deanna Storey</li>
+                <li class="list-group-item">Elizabeth Marvel</li>
+                <li class="list-group-item"> Laura Odeh</li>
+                <li class="list-group-item">Mark Lotito</li>
+                <li class="list-group-item">Daisy Tahan</li>
+                <li class="list-group-item">Erica Fae</li>
+                <li class="list-group-item">Raymond Angelic Sr.</li>
+                <li class="list-group-item">Cliff Carpenter</li>
+                <li class="list-group-item">Timothy Doyle</li>
+                <li class="list-group-item">Amy Spanger</li>
+                <li class="list-group-item"> Nicholas Wyman</li>
+                <li class="list-group-item">Portia</li>
+                <li class="list-group-item">Dan Ziskie</li>
+                <li class="list-group-item">Chris McGinn</li>
+                <li class="list-group-item">Gerald Emerick</li>
+                <li class="list-group-item">Alvin Epstein</li>
+                <li class="list-group-item">Rosemary Murphy</li>
+
+                <li class="list-group-item">Tim Guinee</li>
+                <li class="list-group-item">Kristen Bush</li>
+                <li class="list-group-item">Greg McFadden</li>
+                <li class="list-group-item">Barbara Haas</li>
+                <li class="list-group-item">William Ryall</li>
+                <li class="list-group-item">Joe Lisi</li>
+                <li class="list-group-item">Alice Drummond</li>
+                <li class="list-group-item">Michael Higgins</li>
+                <li class="list-group-item">Stanley Krajewski</li>
+                <li class="list-group-item">Tom Greer</li>
+                <li class="list-group-item">Christopher Evan Welch</li>
+                <li class="list-group-item">Michael Medeiros</li>
+                <li class="list-group-item">John Borras</li>
+                <li class="list-group-item">Brie Eley</li>
+                <li class="list-group-item">Alan Gary</li>
+                <li class="list-group-item">Don Gomez</li>
+                <li class="list-group-item">Brigitte Hagerman</li>
+                <li class="list-group-item">Scott Hatfield</li>
+                <li class="list-group-item">Takako Haywood</li>
+                <li class="list-group-item">Kyle Knauf</li>
+                <li class="list-group-item">David Kneeream</li>
+                <li class="list-group-item">Pete LaRocca</li>
+                <li class="list-group-item">Amelia Martin</li>
+                <li class="list-group-item">Rebecca Merle</li>
+                <li class="list-group-item">Brenna Palughi</li>
+                <li class="list-group-item">Sal Rinella</li>
+                <li class="list-group-item">Tomasz Smolarski</li>
+                <li class="list-group-item">Misha Zubarev</li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Film Center</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M"
+        crossorigin="anonymous">
+    <link rel="stylesheet" href="css/mymovie.css">
+    <link rel="icon" type="image/png" href="img/favicon.png">
+</head>
+
+<body>
+<?php include 'nav.php'; ?>
+    
+    <div class="container">
+        <h1 class="movie_title">Synecdoche, New York</h1>
+        <img class="hero" src="img/sny_hero.jpg" alt="Synecdoche, New York cover image">
+    </div>
+    <div class="container coffee">
+
+        <div class="container">
+            <h4 class="plot">Plot</h4>
+            <p>A successful New York theatre director, Cotard, struggles to create a career-defining play about his own life,
+                examining themes of life, death, futility, and solipsism. The city of New York is struk by disaster as as
+                he creates a life-size replica of the city inside a warehouse as part of his new play. The film focuses on
+                several women in his life, each of them highlighting an aspect of Cotard's own psyche.</p>
+        </div>
+<div class="container">
+        <iframe width="420" height="315"
+        src="https://www.youtube.com/watch?v=XIizh6nYnTU">
+        </iframe>
+
+</div>
+        <div class="container director">
+            <div class="card" style="width: 25rem;">
+                <img class="card-img-top" src="img/kaufman_img.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">Directed By:</h4>
+                    <p class="card-text">Charlie Kaufman</p>
+                    <a href="http://www.imdb.com/name/nm0442109/?ref_=nv_sr_2" class="btn btn-primary">IMDB</a>
+                </div>
+            </div>
+        </div>
+
+
+
+        <div class="container">
+            <div class="d-flex justify-content-end">
+                <p>designed by edwina</p>
+            </div>
+        </div>
+</body>
+
+</html>
